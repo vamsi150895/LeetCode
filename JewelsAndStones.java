ba0333093933cdde27b0a084f3cae7d924f0ff8f
Varun Teja <vamsi150895@gmail.com>
2019-09-29 17:32:11 -0500

    Intial version - needs improvement

--- JewelsAndStones.java
+++ JewelsAndStones.java
@@ -1,9 +1,26 @@
-// First problem from leet code
-
 public class JewelsAndStones{
-	public static void main(String [] args){
-		System.out.print("Test run for git.");
-	
+
+	public int stonesInJewel(String stone, String jewel){
+		int count =0;
+		for (int i = 0 ; i < stone.length(); i++)
+		{
+			for (int j=0 ; j < jewel.length(); j++)
+			{
+				if(stone.charAt(i) == jewel.charAt(j)){
+					count = count + 1;
+				}
+			}
+		}
+		System.out.print(count);
+		return count;
 	}
-	
+
+	public static void main(String [] args){
+		String stone = "aA";
+		String jewel = "aAAbbbB";
+		JewelsAndStones countStones = new JewelsAndStones();
+		countStones.stonesInJewel(stone, jewel);
+
+	}
+
 }
