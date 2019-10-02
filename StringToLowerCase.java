14df858eb44504c085326dd1b958f94edab50fb6
Varun Teja <vamsi150895@gmail.com>
2019-10-02 18:23:34 -0500

    Convert a givens string to Lower Case

---
+++ StringToLowerCase.java
@@ -0,0 +1,12 @@
+
+public class StringToLowerCase {
+	public String toLower(String str){
+		
+		return str.toLowerCase();
+	}
+	public static void main(String [] args){
+		StringToLowerCase str = new StringToLowerCase();
+		str.toLower("HeLlo");
+		
+	}
+}
